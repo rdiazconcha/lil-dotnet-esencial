903188d10e2d4e10e6825cc998307150c6f8eaa6
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-14 14:41:52 +0200

    Se agrega el módulo 4

---
+++ modulo4/tiposdinamicos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tiposdinamicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new
+            {
+                Nombre = ".NET esencial",
+                Autor ="Rodrigo Díaz Concha",
+                Descripcion = "Curso de .NET esencial"
+            };
+
+            Console.WriteLine(c.Nombre);
+            Console.WriteLine(c.Autor);
+            Console.WriteLine(c.Descripcion);
+        }
+    }
+}
---
+++ modulo4/tiposporvaloryreferencia/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tiposporvaloryreferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new Data();
+            data.Number = 10;
+
+            var data2 = data;
+            Console.WriteLine(data.Number);
+            Console.WriteLine(data2.Number);
+
+            Console.WriteLine();
+
+            data2.Number = 20;
+            Console.WriteLine(data.Number);
+            Console.WriteLine(data2.Number);
+        }
+    }
+
+    class Data
+    {
+        public int Number;
+    }
+}
